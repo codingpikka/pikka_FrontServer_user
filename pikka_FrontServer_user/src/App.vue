d3e094300644173e477eba986fe254ca16671061
wsy <wsy9088@gmail.com>
2024-08-02 15:49:54 +0900

    feat app.vue

--- pikka_FrontServer_user/src/App.vue
+++ pikka_FrontServer_user/src/App.vue
@@ -1,3 +1,6 @@
+
+
+
 <template>
     <div id="app">
         <router-view name="header"></router-view>
@@ -5,16 +8,36 @@
             <fade-transition origin="center" mode="out-in" :duration="250">
                 <router-view/>
             </fade-transition>
+            
         </main>
         <router-view name="footer"></router-view>
+        <button @click="handler">등록하기 </button>
     </div>
-</template>
-<script>
-import { FadeTransition } from "vue2-transitions";
 
-export default {
-  components: {
-    FadeTransition
-  }
-};
+</template>
+
+<script setup>
+// import { FadeTransition } from "vue2-transitions";
+
+// export default {
+//   components: {
+//     FadeTransition,
+//     methods: {
+//       handler : ()=>{
+//         console.log("dfdf")
+//       }
+//     },
+//   }
+// };
+
+
+const handler = () =>{
+  fetch("http://localhost:8083/api/hello")
+  .then(reponse => reponse.json())
+  .then(data => {
+    console.log(data);
+    
+  })
+}
+
 </script>
