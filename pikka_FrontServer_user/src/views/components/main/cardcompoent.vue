bb5165582a94cf7cae37dc4c1d00d5d571b55ba5
hwangjeonghan <hjeonghan0@gmail.com>
2024-08-07 17:41:07 +0900

    fix : update card detail

--- pikka_FrontServer_user/src/views/components/main/cardcompoent.vue
+++ pikka_FrontServer_user/src/views/components/main/cardcompoent.vue
@@ -3,76 +3,20 @@
     <div class="row row-cols-1 row-cols-md-3 g-4">
       <div
         class="col"
-        style="border: 1px  solid black; margin: 15px; box-shadow: 5px 5px 5px;"
+        v-for="card in cards"
+        :key="card.id"
+        @click="goToDetail(card.id)"
+        style="border: 1px solid black; margin: 15px; box-shadow: 5px 5px 5px; cursor: pointer;"
       >
         <div class="card h-100">
           <img
-            src="../../../../public/img/brand/mang.png"
+            :src="card.img"
             class="card-img-top"
             alt="..."
           />
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div
-        class="col"
-        style="border: 1px  solid black; margin: 15px; box-shadow: 5px 5px 5px;"
-      >
-        <div class="card h-100">
-          <img
-            src="../../../../public/img/brand/mang.png"
-            class="card-img-top"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a short card.</p>
-          </div>
-        </div>
-      </div>
-      <div
-        class="col"
-        style="border: 1px  solid black; margin: 15px; box-shadow: 5px 5px 5px;"
-      >
-        <div class="card h-100">
-          <img
-            src="../../../../public/img/brand/mang.png"
-            class="card-img-top"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div
-        class="col"
-        style="border: 1px  solid black; margin: 15px; box-shadow: 5px 5px 5px;"
-      >
-        <div class="card h-100">
-          <img
-            src="../../../../public/img/brand/mang.png"
-            class="card-img-top"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
+            <h5 class="card-title">{{ card.title }}</h5>
+            <p class="card-text">{{ card.text }}</p>
           </div>
         </div>
       </div>
@@ -81,7 +25,25 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      cards: [
+        { id: 1, img: require('../../../../public/img/brand/mang.png'), title: 'Card title 1', text: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.' },
+        { id: 2, img: require('../../../../public/img/brand/mang.png'), title: 'Card title 2', text: 'This is a short card.' },
+        { id: 3, img: require('../../../../public/img/brand/mang.png'), title: 'Card title 3', text: 'This is a longer card with supporting text below as a natural lead-in to additional content.' },
+        { id: 4, img: require('../../../../public/img/brand/mang.png'), title: 'Card title 4', text: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.' }
+      ]
+    };
+  },
+  methods: {
+    goToDetail(id) {
+      this.$router.push({ name: 'certificationDetail', params: { id } });
+    }
+  }
+};
 </script>
 
-<style></style>
+<style>
+/* 스타일링은 필요에 따라 추가하세요 */
+</style>
